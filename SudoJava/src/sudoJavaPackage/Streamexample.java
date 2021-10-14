db0d825d93c646deab5bd03cb224d077e10d2fbc
WaleedAhmed05 <waloo_waleed@yahoo.com>
2021-10-14 13:15:23 -0400

    test

--- SudoJava/src/sudoJavaPackage/Streamexample.java
+++ SudoJava/src/sudoJavaPackage/Streamexample.java
@@ -7,8 +7,8 @@ public class Streamexample {
 
 	public static void main(String[] args) {
 		
-		List<Integer> numbers = Arrays.asList(1,2,3);
-		List<Integer> number2 = Arrays.asList(3,4);
+//		List<Integer> numbers = Arrays.asList(1,2,3);
+//		List<Integer> number2 = Arrays.asList(3,4);
 //		List<int[]> pairs = numbers1.stream()
 //				.flatmap(i ->numbers2.stream()
 //				.map(j -> new int[] {i,j}))
