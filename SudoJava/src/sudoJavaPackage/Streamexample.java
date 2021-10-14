71f06d1e63b03de661b1ab139d9c90b707588497
WaleedAhmed05 <waloo_waleed@yahoo.com>
2021-10-14 12:55:19 -0400

    Lab #03, Task #01, and Task 02

---
+++ SudoJava/src/sudoJavaPackage/Streamexample.java
@@ -0,0 +1,18 @@
+package sudoJavaPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Streamexample {
+
+	public static void main(String[] args) {
+		
+		List<Integer> numbers = Arrays.asList(1,2,3);
+		List<Integer> number2 = Arrays.asList(3,4);
+//		List<int[]> pairs = numbers1.stream()
+//				.flatmap(i ->numbers2.stream()
+//				.map(j -> new int[] {i,j}))
+//				.collect(toList());
+				
+	}
+}
