7dc60054e6548be1d205432effac11012f967d5e
WaleedAhmed05 <waloo_waleed@yahoo.com>
2021-12-25 17:50:25 -0500

    Inheritance

---
+++ SudoJava/src/pps/Inheritance01.java
@@ -0,0 +1,25 @@
+package pps;
+
+class superclass {
+	
+	public int sum(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	public void printer() {
+		System.out.println("Test message");
+		}
+	
+	
+}
+
+public class Inheritance01 extends superclass {
+	
+	public static void main (String args[]) {
+		//sum(10,20);
+		
+		System.out.println();
+		
+	}
+
+}
