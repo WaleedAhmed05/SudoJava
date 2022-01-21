5e1f850f6e3ffa1d2995a4e257778061b2099717
WaleedAhmed05 <waloo_waleed@yahoo.com>
2022-01-20 19:26:18 -0500

    Rectangle Quiz.

---
+++ SudoJava/src/pps_week02/Main.java
@@ -0,0 +1,60 @@
+package pps_week02;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		//args=new String[5];
+		//args[0]="hellow";
+		//System.out.println(args.length);
+	//	Child baby = new Child();
+	//	baby.printer();
+		
+	//	Automatic auto = new Automatic();
+		//System.out.println(Rectangle(10,10));
+		Rectange r= new Rectange(10,10);
+		System.out.println(r.toString());
+		
+	} 
+	
+	public void mymethod() {
+		//TODO
+	}
+	
+	public static int Rectangle(int a, int b) {
+		//int var1=a;
+		//int var2=b;
+		
+		return a*b;
+	}
+
+}
+
+
+class Parent {
+public void hello() {
+//System.out.println(“Hello from parent”);
+	System.out.println("Hello Fron Parent");
+
+}
+
+protected int balance() {
+	return 100;
+}
+}
+
+ class Child extends Parent {
+public void hello() {
+//System.out.println(“Hello from Child”);
+	//System.out.println("Hello from Child");
+	
+}
+protected int balance() {
+	//super.balance();
+	return 10;
+	
+}
+public void printer() {
+	System.out.println("Child class "+balance());
+	System.out.println("Parent Class "+super.balance());
+}
+}
---
+++ SudoJava/src/pps_week02/Rectange.java
@@ -0,0 +1,19 @@
+package pps_week02;
+
+public class Rectange {
+	
+	int var1, var2, result;
+	public Rectange(int a, int b) {
+		
+		
+		
+		var1=a;
+		var2=b;
+		result=a*b;
+		//System.out.println(result);
+		
+		
+	}
+	
+
+}
