e67c5f86dd83d1cee7b33317bea6018f7478878f
WaleedAhmed05 <waloo_waleed@yahoo.com>
2022-03-11 00:39:04 -0500

    PPS notes

---
+++ SudoJava/src/lABS/PPS.java
@@ -0,0 +1,26 @@
+package lABS;
+
+/*
+ //Number of occurance of an element.
+ 
+ 
+ Map<Integer, Integer> mp = new HashMap<>(); 
+       // Traverse through array elements and count frequencies
+       for (int i = 0; i < n; i++) 
+        { 
+            if (mp.containsKey(arr[i]))  
+            { 
+                mp.put(arr[i], mp.get(arr[i]) + 1); 
+            }  
+            else
+            { 
+                mp.put(arr[i], 1); 
+            } 
+        } 
+
+
+*/
+
+//----------------------------------------------------------
+//Arrays.sort(charArray);   --Sorting Arrays.
+//Arrays.sort(charArray2, 2, 5); --Sort after specific index to specific index.
