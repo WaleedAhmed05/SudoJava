3e8364554d81ff196c82a33969a993d34f3af675
WaleedAhmed05 <waloo_waleed@yahoo.com>
2022-03-10 23:47:36 -0500

    HashMapAccess..

--- SudoJava/src/lABS/HashMapAccess.java
+++ SudoJava/src/lABS/HashMapAccess.java
@@ -2,11 +2,24 @@ package lABS;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class HashMapAccess {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc= new Scanner(System.in);
+		Scanner sc2= new Scanner(System.in);
+		
+		int noOfTimes=Integer.valueOf(sc.next());
+		String[] lines= new String[noOfTimes];
+		
+		
+		for(int i=0; i<noOfTimes; i++) {
+			lines[i]= sc2.nextLine();
+		}
+	
+	
 		HashMap<String, Integer> map= new HashMap<String, Integer>();
 		
 		for( Map.Entry<String, Integer> s : map.entrySet() ){
